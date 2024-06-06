c07811b2499d46e849453eb048ede9512f2e9945
duonganhthu43 <duonganhthu43@gmail.com>
2024-06-06 14:50:11 +0700

    fix: seed command

--- build.sh
+++ build.sh
@@ -9,4 +9,8 @@ rm -f .jupyterlite.doit.db
 # Run jupyter lite build with output directory as dist
 poetry run jupyter lite build --output-dir dist
 cp "notebooks/read-only.css" "dist/notebooks/read-only.css"
-sed -i '' 's|</head>|<link rel="stylesheet" type="text/css" href="/notebooks/read-only.css" /></head>|' dist/notebooks/index.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's|</head>|<link rel="stylesheet" type="text/css" href="/notebooks/read-only.css" /></head>|' dist/notebooks/index.html
+else
+    sed -i 's|</head>|<link rel="stylesheet" type="text/css" href="/notebooks/read-only.css" /></head>|' dist/notebooks/index.html
+fi
