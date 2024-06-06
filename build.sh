0352bf89f685a77b9692ce4b29aeab5f454f551c
duonganhthu43 <duonganhthu43@gmail.com>
2024-06-06 11:39:53 +0700

    chore: remove seed in build scripts

--- build.sh
+++ build.sh
@@ -8,21 +8,3 @@ rm -f .jupyterlite.doit.db
 
 # Run jupyter lite build with output directory as dist
 poetry run jupyter lite build --output-dir dist
-
-echo "==== Built successfully"
-# Define the source and destination directories
-src="dist/notebooks"
-dst="dist/notebooks-readonly"
-
-# Create the destination directory if it doesn't exist
-mkdir -p "$dst"
-
-# Copy the contents of the source directory to the destination directory
-cp -R "$src/" "$dst"
-
-# Copy the readonly.css file to the destination
-cp "notebooks/read-only.css" "dist/notebooks-readonly/read-only.css"
-
-# Inject the CSS into the HTML files
-
-sed -i '' 's/<\/head>/<link rel="stylesheet" type="text\/css" href="read-only.css" \/><\/head>/' dist/notebooks-readonly/index.html
