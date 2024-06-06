94374d028d5578dfb0509e14baf6ccac2e690057
duonganhthu43 <duonganhthu43@gmail.com>
2024-06-06 14:35:11 +0700

    chore: update css

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ rm -f .jupyterlite.doit.db
 
 # Run jupyter lite build with output directory as dist
 poetry run jupyter lite build --output-dir dist
-
-sed -i '' 's/<\/head>/<link rel="stylesheet" type="text\/css" href="read-only.css" \/><\/head>/' dist/notebooks/index.html 
+cp "notebooks/read-only.css" "dist/notebooks/read-only.css"
+sed -i '' 's|</head>|<link rel="stylesheet" type="text/css" href="/notebooks/read-only.css" /></head>|' dist/notebooks/index.html
