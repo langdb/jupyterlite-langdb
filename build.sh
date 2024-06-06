c02e142ea9df87824f095733d0db5d9fad0cdb94
duonganhthu43 <duonganhthu43@gmail.com>
2024-06-06 13:51:26 +0700

    chore: inject css

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ rm -f .jupyterlite.doit.db
 
 # Run jupyter lite build with output directory as dist
 poetry run jupyter lite build --output-dir dist
+
+sed -i '' 's/<\/head>/<link rel="stylesheet" type="text\/css" href="read-only.css" \/><\/head>/' dist/notebooks/index.html 
