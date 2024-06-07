0e6c8f6ba4c56d5da548ac0f3eab27e7f3a8a447
duonganhthu43 <duonganhthu43@gmail.com>
2024-06-07 14:07:25 +0700

    fix: check exist

--- build.sh
+++ build.sh
@@ -17,6 +17,8 @@ cp -R "$src/" "$dst"
 cp "notebooks/notebooks.css" "dist/notebooks/notebooks.css"
 cp "notebooks/samples.css" "dist/notebooks-samples/samples.css"
 
+ls -la dist/notebooks-samples/
+
 
 if [ "$OSTYPE" == "darwin"* ]; then
     sed -i '' 's|</head>|<link rel="stylesheet" type="text/css" href="/notebooks/notebooks.css" /></head>|' dist/notebooks/index.html
